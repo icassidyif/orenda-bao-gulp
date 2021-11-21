724fed887b0f50f47e7d97bd6c89fd1e6cd2580c
icassidyif <icassidyif@gmail.com>
2021-11-21 20:56:29 +0200

    dev-3

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import '@vendor/testWebp'
 import '@vendor/move'
+import '../blocks/modules/showcase/showcase'
 import '../blocks/modules/gallery/gallery'
 import '../blocks/modules/cookies/cookies'
 import './vendor/validate'
